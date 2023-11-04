b47112950620d1e68ddbe2a08ae5b3a23210bb69
Victor Muniz <victorrmunizz@gmail.com>
2023-11-04 04:23:40 -0300

    implementa migrations

---
+++ WiredBrainCoffee.EmployeeManager/Data/EmployeeManagerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.EmployeeManager.Data.Models;
+
+namespace WiredBrainCoffee.EmployeeManager.Data
+{
+    public class EmployeeManagerDbContext : DbContext
+    {
+        public EmployeeManagerDbContext(
+            DbContextOptions<EmployeeManagerDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+        public DbSet<Department> Departments => Set<Department>();
+    }
+}
--- WiredBrainCoffee.EmployeeManager/Program.cs
+++ WiredBrainCoffee.EmployeeManager/Program.cs
@@ -1,19 +1,31 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.EmployeeManager.Data;
 
 namespace WiredBrainCoffee.EmployeeManager
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
+            builder.Services.AddDbContext<EmployeeManagerDbContext>(
+                opt => opt.UseSqlServer(
+                    builder.Configuration.GetConnectionString("EmployeeManagerDb")));
 
             var app = builder.Build();
+
+            await EnsureDatabaseIsMigrated(app.Services);
+
+            async Task EnsureDatabaseIsMigrated(IServiceProvider services)
+            {
+
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
